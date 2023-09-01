bb6c2037729de99db3ceb2872d59cf54c3e112e6
Hemalatha827 <67148302+Hemalatha827@users.noreply.github.com>
2023-09-01 16:15:20 +0530

    Delete strapi/my-strapi-project directory

--- strapi/my-strapi-project/src/plugins/todo/admin/src/components/PluginIcon/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Brush } from '@strapi/icons/Brush';
-
-const PluginIcon = () => <Brush />;
-
-export default PluginIcon;
--- strapi/my-strapi-project/src/plugins/todo/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-// server/routes/index.js
-const task = require('./task');
-const report = require('./report');
-
-module.exports = {
-  task,
-  report,
-};
